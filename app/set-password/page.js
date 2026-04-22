ac8cfede7e43c977325cf970fa523ab4cbe63d10
viktoria-stack <viktoria@tailoredathlete.com>
2026-04-22 14:27:37 +0200

    Update page.js

--- app/set-password/page.js
+++ app/set-password/page.js
@@ -1,9 +1,9 @@
 'use client'
 import { useState } from 'react'
-import { createBrowserClient } from '@supabase/ssr'
+import { createClient } from '@supabase/supabase-js'
 import { useRouter } from 'next/navigation'
 
-const supabase = createBrowserClient(
+const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
